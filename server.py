8f534c404fba9421de72ebb583ddf97d38e483a1
Mike Dacanay <migd64@gmail.com>
2017-08-03 19:12:15 -0700

    finished proto, user info posts correctly, remove render_template function under create_user() when done testing

--- server.py
+++ server.py
@@ -5,17 +5,11 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
-
-@app.route('/process', methods=['POST'])
+@app.route('/result', methods=['POST'])
 def create_user():
 	name=request.form['name']
 	location=request.form['location']
 	language=request.form['language']
 	comment=request.form['comment']
-	return redirect('/result')
-
-@app.route('/result')
-def result():
 	return render_template('result.html', name1=name, location1=location, favorite_language1=language, comment1=comment)
-
 app.run(debug=True)
