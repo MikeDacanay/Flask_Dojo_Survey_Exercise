48b7fe070b251cf0d9c7cabb38f2b12397b1b3e9
Mike Dacanay <migd64@gmail.com>
2017-08-06 16:37:59 -0700

    added basic validations to comment and name field

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, session, flash
 app = Flask(__name__)
+app.secret_key= "Jade"
 
 @app.route('/')
 def index():
@@ -11,5 +12,14 @@ def create_user():
 	location=request.form['location']
 	language=request.form['language']
 	comment=request.form['comment']
+
+	if len(request.form['name'])<1:
+		flash("Name Field Empty!")
+
+	if len(request.form['comment'])>=120:
+		flash("Your comment is too long!")
+
+
+
 	return render_template('result.html', name1=name, location1=location, favorite_language1=language, comment1=comment)
 app.run(debug=True)
